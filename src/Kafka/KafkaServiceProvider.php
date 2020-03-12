6632898f06deb07b0d2d1fdd50a2de3bfdbcfd57
costalong <longqiuhong@163.com>
2020-03-12 11:39:01 +0800

    init

---
+++ src/Kafka/KafkaServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Date: 2020-03-12
+ * Time: 10:54
+ * author: costalong
+ * email: longqiuhong@163.com
+ */
+
+namespace Costalong\Kafka;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class KafkaServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * author: costalong
+	 * email: longqiuhong@163.com
+	 */
+	public function boot()
+	{
+
+	}
+}
